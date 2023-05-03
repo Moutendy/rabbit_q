c212959cd66fdd004116ed74a995dcfc7de2a005
cmoutendy <cmoutendy@caf-technologies.com>
2023-05-03 20:11:24 +0200

    first commit

---
+++ src/main/java/rabbit/mq/controller/TesteController.java
@@ -0,0 +1,22 @@
+package rabbit.mq.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import rabbit.mq.componenet.QueueSender;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+	@Autowired
+    private QueueSender queueSender;
+
+    @GetMapping
+    public String send(){
+        queueSender.send("test message");
+        return "ok. done";
+    }
+
+}
